5069993377807c16da0360921dbcebf9d16b7c92
tmyshrall <149275543+tmyshrall@users.noreply.github.com>
2024-01-05 18:09:43 -0500

    Add files via upload

---
+++ Random.js
@@ -0,0 +1,39 @@
+var textArray = [
+  "Tyler",
+  "Jonathan",
+  "Aditya",
+  "Ryan",
+  "Ryker",
+];
+
+function startCountdown() {
+  var countdownElement = document.getElementById("countdown");
+  var count = 4;
+
+  function updateCountdown() {
+    countdownElement.textContent = count;
+
+    if (count <= 0) {
+      clearInterval(countdownInterval);
+      displayRandomName();
+    } else {
+      
+      countdownElement.style.animation = 'none';
+      void countdownElement.offsetWidth;
+      countdownElement.style.animation = null; 
+    }
+
+    count--;
+  }
+
+  var countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+function displayRandomName() {
+  var countdownElement = document.getElementById("countdown");
+  var randomName = textArray[Math.floor(Math.random() * textArray.length)];
+  countdownElement.textContent = randomName;
+}
+
+window.onload = startCountdown;
+
